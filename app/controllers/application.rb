85f56afaa002920b87a8b77b667ace6fd7585b94
Chris Bunkers <christopher.bunkers@gmail.com>
2016-06-23 15:16:21 -0500

    clean up

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,9 +1,4 @@
-options '/*' do
-    headers['Access-Control-Allow-Origin'] = "*"
-    headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
-    headers['Access-Control-Allow-Headers'] ="accept, authorization, origin"
-end
 get '/' do
-  erb :'index'
+  erb :'/baseball/index'
 end
 
